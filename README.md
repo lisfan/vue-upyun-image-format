127caf5c10ef1fb27da1d0c6d1f5041267c1a242
lisfan <goolisfan@gmail.com>
2017-10-30 15:01:59 +0800

    chore: 更新依赖版本限定符

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# vue-upyun-image-clipper
-vue过滤器插件-又拍云图片裁剪器：根据DPR尺寸处理显示的图片大小
+# logger 日志打印器
+
+[API documentation](https://lisfan.github.io/logger/)
+
+## Feature 特性
+
+- 解决提交时因eslint提示console的语句无法通过问题
+- 仅在开发环境打印日志，生产环境不打印日志
+
+## Detail 详情
+
+- 在console上包装了一层，支持console的所有的方法（包含部分非标准APi，但不包含未被废弃的API），部分API做了变化和新增加，未提及的保原效果不变，只是在原api上封装了一层进行代理运行，API使用方法可以参考[console API](https://developer.mozilla.org/en-US/docs/Web/API/Console/group)
+  - 新增的isActivated、color、enable、disable方法
+  - 调整error方法的作用：打印时会抛出错误，阻止脚本执行
+  - 调整table方法的作用：如果数据非array或object类型，则使用this.log打印
+- 若需要在生产环境下调式日志，可以更改或设置LS离线存储的值
+   - localStorage设置`IS_DEV`为true
+   - localStorage设置`LOGGER_RULES`配置命名空间规则
+- 支持配置整个命名空间是否输出日志
+- 支持配置命名空间下某个实例方法是否输出日志
+
+## Install 安装
+
+```bash
+npm install -S @~lisfan/logger
+```
+
+## Usage 起步
+
+``` js
+import Logger from '@~lisfan/logger'
+
+// 配置规则
+Logger.configRules({
+   request:true, // 该命名空间支持打印输出
+   request.error:false, // 该命名空间下的error方法不支持打印输出
+   response:false // 该命名空间不支持打印输出
+})
+
+const logger = new Logger() // 默认打印器，命名空间为`logger`
+const loggerRequest = new Logger('request') // 创建命名空间为`request`的打印器
+const loggerResponse = new Logger('response')
+
+// 创建打印器，但关闭调试功能
+const loggerDebug = new Logger({
+   name: 'debug',
+   debug: false
+})
+
+loggerRequest.log('请求url')    =>    [request]: 请求url
+loggerRequest.error('请求url')    =>    // 无内容打印
+loggerResponse.error('响应数据')    =>    // 无内容打印
+loggerDebug.log('请求url')    =>     // 无内容打印
+```
+
+
+ *
+ * 模块性质：vue 过滤器 插件
+ * 作用范围：pc、mobile
+ * 依赖模块：lodash，utils/logger
+ * 来源项目：扫码点单H5
+ * 初始发布日期：2017-05-24 20:00
+ * 最后更新日期：2017-05-25 20:00
+ *
+ * ## 特性
+ * - 根据当前设计稿和设备物理分辨率缩放比，及设备dpr值，计算出实际的图片尺寸
+ * - 使用的是又拍云图片服务。specs规格参数和附加规则参考又拍云 http://docs.upyun.com/cloud/image/#webp
+ * - 图片尺寸值会四舍五入取整
+ * - 图片尺寸的裁剪规则
+ *   - 如果是wifi网络或者未知网络，统一使用当前设备dpr计算后的图片尺寸
+ *   - 如果是4g网络，最大dpr值只取到dpr为3（可自定义），防止图片过大造成加载时间过久
+ * - 使用格式： <img :src="图片地址 | image-size(size,[specs = 'both'],[rule])" />
+ *
+ * ## Todo
+ *
+ * ## Changelog
+ *
+ * ## Usage
+ *
+ * // 现有需求如下：
+ * // 1. 当前设计稿件750px宽，物理分辨率350px，缩放比为2倍，当前dpr值是3，设计稿里的图片占位区域尺寸300x400
+ * // 2. 此时尺寸的计算公式为，宽：300*3/(750/350)，高：400*3/(750/350)，最终尺寸为450x600
+ * // 3. 原图片尺寸如果小于450时不进行放大，但如果图片尺寸大于450，则缩小到450
+ * // 4. 在图片上加上水印'你好'
+ *
+ * ```html
+ *  <img :image-src="coverUrl | image-size('300x300','fwfh','/watermark/text/5L2g5aW977yB')" />
+ * ```
---
+++ src/index.js
@@ -0,0 +1,83 @@
+/**
+ * 又拍云图片尺寸裁切
+ *
+ * @version 1.0.0
+ */
+
+import _ from 'lodash'
+import Logger from 'utils/logger'
+
+const imageSize = {} // 插件对象
+const filterName = 'image-size' // 过滤器名称
+const logger = new Logger(`filter-${filterName}`)
+
+/**
+ * 暴露install钩子，供vue注册
+ * @param {Vue} Vue - Vue构造器类
+ * @param {object} [options={}] - 插件配置参数
+ * @param {number} [max4gDpr=3] - 4g网络下dpr值取的最大值，默认值为3
+ * @param {number} [draftRatio=2] - ui尺寸和设备物理尺寸的比例，默认值为2
+ * @param {string} [specs='both'] - 裁剪方式
+ * @param {string} [rule=''] - 其他规则，需要以'/'开头，附加上其他规则
+ */
+imageSize.install = function (Vue, {
+  max4gDpr = 3,
+  draftRatio = 2,
+  specs = 'both',
+  rule = ''
+} = {}) {
+  /**
+   * 接受四个参数
+   * @param {number|string} size - 裁剪的图片尺寸
+   * @param {string} specsType - 又拍云图片裁切规格
+   * @param {string} rule - 除图片裁切方式外的，其他又拍云规则
+   */
+  Vue.filter(filterName, (value, size, specsType = specs, otherRule = rule) => {
+    if (_.isUndefined(value)) {
+      return ''
+    }
+    // 非数字和图片时提示参数错误
+    if (!_.isNumber(size) && !_.isString(size)) {
+      logger.error(`${filterName} require a size param`)
+      return false
+    }
+
+    const docHtml = document.documentElement
+    const dpr = Number.parseFloat(docHtml.getAttribute('data-dpr')) || 1
+    const networkType = docHtml.getAttribute('data-network-type') || 'unknow'
+    const ratio = draftRatio || Number.parseFloat(docHtml.getAttribute('data-draft-ratio'))
+
+    logger.log('图片地址为value', value)
+    logger.log('图片裁剪尺寸为size', size)
+    logger.log('图片裁剪方式为specsType', specsType)
+    logger.log('其他规格附加otherRule', otherRule)
+    logger.log('当前设备dpr值', dpr)
+    logger.log('当前设备网络制式', networkType)
+    logger.log('当前设计稿与设备比', ratio)
+
+    let finalRatio
+    let finalSize
+
+    if ((networkType === 'unknow' || networkType === 'wifi')) {
+      finalRatio = dpr
+    } else if (networkType === '4g') {
+      finalRatio = dpr >= max4gDpr ? max4gDpr : dpr
+    }
+
+    const sizeList = size.toString().split('x')
+
+    // 计算尺寸
+    const finalSizeList = sizeList.map((sizeItem) => {
+      return Math.round((Number.parseFloat(sizeItem) / ratio) * finalRatio)
+    })
+
+    finalSize = finalSizeList.join('x')
+
+    logger.log('最终决定图片尺寸的比例是', finalRatio)
+    logger.log('最终决定图片尺寸的尺寸是', finalSize)
+    logger.log('最终图片url', `${value}!/${specsType}/${finalSize}${otherRule}`)
+    return `${value}!/${specsType}/${finalSize}${otherRule}`
+  })
+}
+
+export default imageSize
