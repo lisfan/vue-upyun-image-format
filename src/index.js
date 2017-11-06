e42971ac463d21892eaa778249cf627899a7e7f7
lisfan <goolisfan@gmail.com>
2017-11-06 16:53:27 +0800

    optimize: 优化主模块的注释和逻辑

--- src/index.js
+++ src/index.js
@@ -13,13 +13,17 @@ import isWebp from './utils/webp-features-support'
 import computeDefaultFormat from './utils/compute-default-format'
 
 let ImageFormat = {} // 插件对象
+const PLUGIN_TYPE = 'filter' // 插件类型
 const FILTER_NAMESPACE = 'image-format' // 过滤器名称
-const PLUGIN_TYPE = 'filter'
 
 // 私有方法
 const _actions = {
   /**
    * 获取图片后缀
+   * 【注】假设图片文件末尾都是以存在后缀的，未兼容处理那些不带后缀形式的图片文件
+   *
+   * @param {string} filename - 文件名称
+   * @returns {string}
    */
   getFileExtension(filename) {
     const extReg = /\.([^.]*)$/
@@ -34,10 +38,10 @@ const _actions = {
   /**
    * 根据当前的网络制式，获取最终的DPR值
    *
-   * @param networkType
-   * @param DPR
-   * @param maxDPR
-   * @returns {*}
+   * @param {string} networkType - 网络制式类型
+   * @param {number} DPR - 设备像素比
+   * @param {number} maxDPR - 支持的最大设备像素比
+   * @returns {number}
    */
   getFinalDPR(networkType, DPR, maxDPR) {
     if (networkType === '4g') {
@@ -49,18 +53,20 @@ const _actions = {
     }
   },
   /**
-   * 获取图片尺寸
-   * @param size
-   * @param finalDPR
-   * @param draftRatio
-   * @returns {string}
+   * 获取最终的图片尺寸
+   *
+   * @param {?number|string} size - 自定义尺寸
+   * @param {number} finalDPR
+   * @param {number} draftRatio
+   * @returns {?string}
    */
   getFinalSize(size, finalDPR, draftRatio) {
+    // 是否存在有效的尺寸值
     let isValidSize = false
+
     let finalSizeList = []
 
-    if (!validation.isNull(size)) {
-      // 计算图片尺寸
+    if (!validation.isNil(size)) {
       const sizeList = size.toString().split('x')
 
       finalSizeList = sizeList.map((sizeItem) => {
@@ -73,47 +79,55 @@ const _actions = {
       })
     }
 
-    return isValidSize ? finalSizeList.join('x') : ''
+    return isValidSize ? finalSizeList.join('x') : null
   },
   /**
-   * 获取图片格式
-   * @param format
+   * 根据条件，获取最终的图片格式
+   * @param {string} format - 自定义格式
+   * @param {string} originFormat - 原格式
+   * @param {number} width - 自定义尺寸
+   * @param {number} minWidth - 最小尺寸
+   * @param {boolean} [lossless] - 是否转换为无损webp格式
+   * @returns {string}
    */
   getFinalFormat(format, originFormat, width, minWidth, lossless) {
-    // 若指定了图片格式，且不是webp格式，则直接返回
-    // 未指定，则使用默认格式
+    // 若未自定义图片格式，则根据一些条件，设置默认格式
+    // 若自定义了图片格式，且不是指定为webp格式，则直接返回指定的格式
     // 若指定为webp格式
-    //   - 则判断源图片gif格式，则判断是否支持动态webp格式，
-    //   - 则判断源图片jpg或png格式，则判断是否支持有损webp格式，
+    //   - 若源图片gif格式，则检测是否支持动态webp格式，
+    //   - 若源图片jpg或png格式，则检测是否支持有损webp格式或无损webp格式
     // 如果指定了自定义值，则使用自定义值
     if (!validation.isString(format)) {
       return computeDefaultFormat(originFormat, width, minWidth)
     }
 
     if (format === 'webp') {
-      // todo 如果是gif则判断是否支持动效
-      // todo 是否强制指定了无损
-      // 如果用户指定了图片格式，则以用户自定义为主
-      // 若不支持，则动态webp转换为gif格式，其他格式统一转换为jpg格式
+      // 若源图片gif格式，则检测是否支持动态webp格式，若不支持则转换为gif格式
+      //   - 若源图片jpg或png格式，则检测是否支持有损webp格式或无损webp格式
       if (originFormat === 'gif' && !isWebp.support('animation')) {
         return 'gif'
-      } else if (originFormat.match(/jpeg|jpg|png/) && !isWebp.support('lossy')) {
+      } else if (originFormat.match(/jpeg|jpg|png/) && !lossless && !isWebp.support('lossy')) {
+        return 'jpg'
+      } else if (originFormat.match(/jpeg|jpg|png/) && lossless && !isWebp.support('lossless')) {
         return 'jpg'
       } else {
         return 'webp'
       }
-    } else {
-      return format
     }
+
+    // 返回源格式
+    return format
   },
 
   /**
-   * 过滤为undefined和null的值
+   * 过滤为undefined、null及空值
+   * @param {object} rules - 规则配置
+   * @returns {object}
    */
   filterRules(rules) {
     let filterRules = {}
     Object.entries(rules).forEach(([key, value]) => {
-      if (!validation.isNil(value)) {
+      if (!validation.isNil(value) && !validation.isEmpty(value)) {
         filterRules[key] = value
       }
     })
@@ -121,9 +135,13 @@ const _actions = {
     return filterRules
   },
   /**
-   * 其他规则项
+   * 获取自定义的其他又拍云规则项
+   * [注] 规则项需要有对应关系，不可随意填写
+   *
+   * @param {object} otherRules- 其他又拍云规则项
+   * @returns {object}
    */
-  getFinalOtherRules(logger, otherRules) {
+  getFinalOtherRules(otherRules) {
     if (validation.isEmpty(otherRules)) {
       return {}
     }
@@ -137,9 +155,9 @@ const _actions = {
 
     const rules = otherRules.split('/')
 
-    // 切割后的长度能被整除
+    // 分割后的的数据长度能被2整除，无余数
     if (rules.length % 2 !== 0) {
-      logger.error('other rules is\'t parse! please check')
+      throw new Error('other rules is\'t parse! please check')
     }
 
     const finalOtherRules = {}
@@ -155,6 +173,9 @@ const _actions = {
   /**
    * 根据图片格式进一步优化规则
    * 目前只有两条规则，所以不采用策略，直接进行逻辑判断
+   *
+   * @param {object} rules - 优化规则
+   * @return {object}
    */
   optimizeRules(rules) {
     // 若未jpg格式，且不存在模糊到清晰配置时，
@@ -167,32 +188,23 @@ const _actions = {
     return rules
   },
   /**
-   * 序列化规则
-   * 值不存在时，则表示wgkqe和默认值
+   * 序列化规则为符合格式的字符串
+   *
    * @param {object} rules - 规则配置
    * @returns {string}
    */
   stringifyRule(rules) {
-    // 合并默认规则配置
-    // rules = {
-    //   ...RULES,
-    //   ..._actions.filterRules(rules),
-    // }
-
-    // 再次过滤
     const filterRules = _actions.filterRules(rules)
-
-    const len = Object.keys(filterRules).length
 
     // 处理针对格式的优化
     rules = _actions.optimizeRules(rules)
 
     // 不存在值时，直接返回空字符串
-    if (len === 0) {
+    if (Object.keys(filterRules).length === 0) {
       return ''
     }
 
-    // 提前取出缩放方式(scale)和尺寸(size)进行额外的处理
+    // 提前取出缩放方式(scale)和尺寸(size)进行额外的处理，其他值做拼接
     let imageSrc = validation.isNil(filterRules.size) ? '' : `/${filterRules.scale}/${filterRules.size}`
 
     imageSrc += Object.entries(filterRules).reduce((result, [key, value]) => {
@@ -203,10 +215,11 @@ const _actions = {
       return result + `/${key}/${value}`
     }, '')
 
-    // 尺寸规格或其他规则至少存在一项时，加上前缀`!`修饰符号
+    // 规则至少存在一项时，则加上前缀`!`修饰符号
     return validation.isEmpty(imageSrc) ? '' : '!' + imageSrc
   }
 }
+
 /**
  * 又拍云图片格式化注册函数
  * 规则请参考[又拍云文档](http://docs.upyun.com/cloud/image/#webp)
@@ -288,7 +301,7 @@ ImageFormat.install = async function (Vue, {
     logger.log('origin image quality:', originQuality)
     logger.log('origin rules:', originOtherRules)
 
-    let finalRules = _actions.getFinalOtherRules(logger, originOtherRules) // 最终的其他规则取值
+    let finalRules = _actions.getFinalOtherRules(originOtherRules) // 最终的其他规则取值
     let finalDPR = _actions.getFinalDPR(networkType, DPR, maxDPR) // 最终的DPR取值
     let finalSize = _actions.getFinalSize(originSize, finalDPR, draftRatio) // 最终的尺寸取值
     let finalFormat = _actions.getFinalFormat(originFormat, _actions.getFileExtension(src), finalSize.split('x')[0], minWidth, finalRules.lossless) // 最终的图片格式
